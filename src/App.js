058edfbc63d0cf54c7c8b27b5dc2cb74b38ea837
haritejakathi <161744983+haritejakathi@users.noreply.github.com>
2024-07-05 10:26:12 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Provider, defaultTheme, Button, TextField } from '@adobe/react-spectrum';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    mockDB: [],
+    inputData: ''
+  };
+
+  handleInputChange = (value) => {
+    this.setState({ inputData: value });
+  };
+
+  handleAddData = () => {
+    this.setState((prevState) => ({
+      mockDB: [...prevState.mockDB, prevState.inputData],
+      inputData: ''
+    }));
+  };
+
+  handleDeleteData = (index) => {
+    this.setState((prevState) => ({
+      mockDB: prevState.mockDB.filter((_, i) => i !== index)
+    }));
+  };
+
+  render() {
+    return (
+      <Provider theme={defaultTheme}>
+        <div className="App">
+          <h1>Hava Havai Frontend Assignment</h1>
+          <TextField
+            label="Input Data"
+            value={this.state.inputData}
+            onChange={this.handleInputChange}
+          />
+          <Button variant="cta" onPress={this.handleAddData}>
+            Add Data
+          </Button>
+          <ul>
+            {this.state.mockDB.map((data, index) => (
+              <li key={index}>
+                {data}
+                <Button variant="secondary" onPress={() => this.handleDeleteData(index)}>
+                  Delete
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
